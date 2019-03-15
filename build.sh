3de65f98c48d409d83d5aad0bac92a3086eab9db
Mario Schlack <hurikhan@protonmail.com>
2019-03-15 12:20:05 +0800

    - moved godot_headers to src/ directory

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #! /usr/bin/env bash
 
+cd src
 if [ ! -d godot_headers ]
 then
 	git clone https://github.com/GodotNativeTools/godot_headers.git
 fi
 
-cd src
 clang-format -i --style=file *.h
 clang-format -i --style=file *.c
 cd ..
 
 mkdir -p bin
-clang -std=c11 -fPIC -c -g -I godot_headers src/gdns.c -o bin/gdns.os && \
-clang -std=c11 -fPIC -c -g -I godot_headers src/gdarchive.c -o bin/gdarchive.os && \
+clang -std=c11 -fPIC -c -g -I src/godot_headers src/gdns.c -o bin/gdns.os && \
+clang -std=c11 -fPIC -c -g -I src/godot_headers src/gdarchive.c -o bin/gdarchive.os && \
 clang -shared -larchive bin/gdns.os bin/gdarchive.os -o bin/gdarchive.so && \
 rm bin/gdns.os && \
 rm bin/gdarchive.os
