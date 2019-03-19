04593ce49054970e11a61e6ed6cc31655095cb83
Mario Schlack <hurikhan@protonmail.com>
2019-03-19 23:12:07 +0800

    - Renamed var from version to info

--- test/0002_get_info.gd
+++ test/0002_get_info.gd
@@ -4,15 +4,15 @@ func _init():
 	var test = preload("test.gd").new()
 
 	var archive = preload("addons/gdArchive/gdArchive.gdns").new()
-	var version = archive.get_info()
+	var info = archive.get_info()
 
-	test.assert_type(version, TYPE_DICTIONARY)
+	test.assert_type(info, TYPE_DICTIONARY)
 
-	test.assert("Dict has key 'gdarchive'", version.has('gdarchive'))
-	test.assert_type(version['gdarchive'], TYPE_STRING)
+	test.assert("Dict has key 'gdarchive'", info.has('gdarchive'))
+	test.assert_type(info['gdarchive'], TYPE_STRING)
 
-	test.assert("Dict has key 'libarchive'", version.has('libarchive'))
-	test.assert_type(version['libarchive'], TYPE_STRING)
+	test.assert("Dict has key 'libarchive'", info.has('libarchive'))
+	test.assert_type(info['libarchive'], TYPE_STRING)
 	
 	test.done()
 	test.free()
